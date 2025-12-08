e7f7a051d45f0a7b317ff99b205441601722a38e
basilkiwanuka <basilkiwanuka@icloud.com>
2025-12-08 14:34:44 -0500

    updated upload-sarif@v2 to upload-sarif@v3

--- catalog-service/src/main/java/com/mbsystems/catalogservice/controller/HomeController.java
+++ catalog-service/src/main/java/com/mbsystems/catalogservice/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.mbsystems.catalogservice.controller;
 
+import com.mbsystems.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog service!";
+        return this.polarProperties.getGreeting();
     }
 }
